7ecb4d716819bb048a50fc3c4f5bac92cfccd0a2
Jian-Yang Li <jianyang2li@gmail.com>
2017-05-31 11:43:10 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ if __name__ == '__main__':
     setup(name='jylipy',
           version='0.1',
           description='JyLi''s Python tools',
-          url='',
+          url='https://jianyangli2017@bitbucket.org/jianyangli2017/jylipy_2to3.git',
           author='Jian-Yang Li',
           author_email='jianyang2li@gmail.com',
           license='MIT',
           packages=['jylipy','jylipy.constants','jylipy.HST','jylipy.mesh','jylipy.Photometry'],
-          zip_safe=False)
-
+          requires=['numpy','scipy','astropy','matplotlib','pyds9','pysis','ccdproc'],
+          zip_safe=False,
+          classifiers = [
+              'Intended Audience :: Science/Research',
+              #"License :: OSI Approved :: BSD License",
+              'Operating System :: OS Independent',
+              "Programming Language :: Python :: 3",
+              'Topic :: Scientific/Engineering :: Astronomy'
+          ]
+          )
