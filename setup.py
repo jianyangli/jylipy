d11774d56b5bc1c087a12743e8ff7b35177e28b4
Jian-Yang Li <jianyang2li@gmail.com>
2017-05-31 11:37:33 -0400

    packaging
    * Packaging the module with setuptools
    * Untrack build and dist related contents

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+
+    setup(name='jylipy',
+          version='0.1',
+          description='JyLi''s Python tools',
+          url='',
+          author='Jian-Yang Li',
+          author_email='jianyang2li@gmail.com',
+          license='MIT',
+          packages=['jylipy','jylipy.constants','jylipy.HST','jylipy.mesh','jylipy.Photometry'],
+          zip_safe=False)
+
