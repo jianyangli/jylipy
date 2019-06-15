7e3a3f04bf3d1e78c0129e0290557ed7ce43bbbe
Jian-Yang Li <jianyang2li@gmail.com>
2019-06-14 21:36:04 -0400

    add parameter name properties PhotometricModelArray

--- jylipy/Photometry/photometricdatagroup.py
+++ jylipy/Photometry/photometricdatagroup.py
@@ -381,6 +381,18 @@ class PhotometricModelArray(np.ndarray):
     def __array_finalize__(self, obj):
         if obj is None: return
         self.__version__ = getattr(obj, '__version__', '1.0.0')
+        self._model_class = getattr(obj, 'model_class', None)
+        self._param_names = getattr(obj, 'param_names', None)
+
+    def __getattr__(self, name):
+        if name not in self.param_names:
+            try:
+                prop = super().__getattr__(name)
+            except AttributeError:
+                raise AttributeError("'{}' object has no attribute '{}'".
+                    format(self.__class__.__name__, name))
+        else:
+            return self.get_param(name)
 
     @property
     def param_names(self):
@@ -437,7 +449,8 @@ class PhotometricModelArray(np.ndarray):
             if isinstance(v, self.model_class):
                 for x in np.nditer(self[k], flags=['refs_ok'],
                         op_flags=['readwrite']):
-                    pars = tuple([getattr(v, p).value for p in self.param_names])
+                    pars = tuple([getattr(v, p).value for p in
+                        self.param_names])
                     x[...] = (False,) + pars
             elif isinstance(v, PhotometricModelArray):
                 for f in list(self.dtype.names):
@@ -452,44 +465,45 @@ class PhotometricModelArray(np.ndarray):
                     ' instance allowed.'.format(self.model_class.__class__.
                     __name__))
 
-    def _init_model_params(self):
-        """Initialize model class using default parameters of self.model_class
+    def get_param(self, p):
+        """Return parameter 'p'
         """
-        for p in self.param_names:
-            self.__dict__[p] = self[p]
-            len_arr = np.ma.zeros(self.shape, dtype=int)
-            len_arr.mask = self.mask
-            it = np.nditer(self, flags=['multi_index', 'refs_ok'])
+        if p not in self.param_names:
+            raise ValueError("'{}' is not a parameter of model '{}'".
+                format(p, self.model_class.__name__))
+        len_arr = np.ma.zeros(self.shape, dtype=int)
+        len_arr.mask = self.mask
+        it = np.nditer(self, flags=['multi_index', 'refs_ok'])
+        while not it.finished:
+            if not len_arr.mask[it.multi_index]:
+                par = self[self.param_names[0]][it.multi_index]
+                if hasattr(par, '__iter__'):
+                    len_arr[it.multi_index] = np.size(par)
+                else:
+                    len_arr[it.multi_index] = 1
+            it.iternext()
+        len_max = len_arr.max()
+        if len_max == 1:
+            # single model
+            return np.ma.array(self[p], mask=self.mask, \
+                dtype=float)
+        elif (len_arr == len_max).all():
+            # homogeneous number of models
+            par = self[p]
+            non_masked = np.where(~self.mask.flatten())[0].min()
+            shape = np.shape(par.flatten()[non_masked])
+            it = np.nditer(par, flags=['multi_index', 'refs_ok'])
             while not it.finished:
-                if not len_arr.mask[it.multi_index]:
-                    par = self[self.param_names[0]][it.multi_index]
-                    if hasattr(par, '__iter__'):
-                        len_arr[it.multi_index] = np.size(par)
-                    else:
-                        len_arr[it.multi_index] = 1
+                if self.mask[it.multi_index]:
+                    par[it.multi_index] = np.zeros(shape)
                 it.iternext()
-            len_max = len_arr.max()
-            if len_max == 1:
-                # single model
-                self.__dict__[p] = np.ma.array(self[p], mask=self.mask, \
-                    dtype=float)
-            elif (len_arr == len_max).all():
-                # homogeneous number of models
-                par = self[p]
-                non_masked = np.where(~self.mask.flatten())[0].min()
-                shape = np.shape(par.flatten()[non_masked])
-                it = np.nditer(par, flags=['multi_index', 'refs_ok'])
-                while not it.finished:
-                    if self.mask[it.multi_index]:
-                        par[it.multi_index] = np.zeros(shape)
-                    it.iternext()
-                par = np.vstack(par.reshape(-1))
-                par = par.reshape(self.shape+shape).astype(float)
-                mask = np.broadcast_to(self.mask, shape+self.shape)
-                ndim = len(mask.shape)
-                for i in range(ndim - len(self.shape)):
-                    mask = np.rollaxis(mask, 0, ndim)
-                self.__dict__[p] = np.ma.array(par, mask=mask)
-            else:
-                # variable number of models
-                self.__dict__[p] = np.ma.array(self[p], mask=self.mask)
+            par = np.vstack(par.reshape(-1))
+            par = par.reshape(self.shape+shape).astype(float)
+            mask = np.broadcast_to(self.mask, shape+self.shape)
+            ndim = len(mask.shape)
+            for i in range(ndim - len(self.shape)):
+                mask = np.rollaxis(mask, 0, ndim)
+            return np.ma.array(par, mask=mask)
+        else:
+            # variable number of models
+            return np.ma.array(self[p], mask=self.mask)
