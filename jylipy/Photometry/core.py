cdf2c79d83f50a406bf64ce4573444f4ca406f05
Jian-Yang Li <jianyang2li@gmail.com>
2021-11-18 22:14:45 -0500

    default dtype to ‘f4’

--- jylipy/Photometry/core.py
+++ jylipy/Photometry/core.py
@@ -98,13 +98,19 @@ class ScatteringGeometry(object):
             self._unit = kwargs.pop('unit')
             par = set(kwargs.keys())
             if set('inc emi pha'.split()) == par:
-                data = [kwargs['inc'], kwargs['emi'], kwargs['pha']]
+                data = [np.asarray(kwargs['inc'], dtype='f4'),
+                        np.asarray(kwargs['emi'], dtype='f4'),
+                        np.asarray(kwargs['pha'], dtype='f4')]
                 names = 'inc emi pha'.split()
             elif set('inc emi psi'.split()) == par:
-                data = [kwargs['inc'], kwargs['emi'], kwargs['psi']]
+                data = [np.asarray(kwargs['inc'], dtype='f4'),
+                        np.asarray(kwargs['emi'], dtype='f4'),
+                        np.asarray(kwargs['psi'], dtype='f4')]
                 names = 'inc emi psi'.split()
             elif set('pha lat lon'.split()) == par:
-                data = [kwargs['pha'], kwargs['lat'], kwargs['lon']]
+                data = [np.asarray(kwargs['pha'], dtype='f4'),
+                        np.asarray(kwargs['lat'], dtype='f4'),
+                        np.asarray(kwargs['lon'], dtype='f4')]
                 names = 'pha lat lon'.split()
             else:
                 raise GeometryError('Scattering geometry combination not recognized.')
@@ -783,25 +789,33 @@ class PhotometricData(object):
             # collect reflectance data
             keys = list(kwargs.keys())
             if 'bdr' in keys:
-                self._data = Table([kwargs['bdr']], names=['BDR'])
+                self._data = Table([np.asarray(kwargs['bdr'], dtype='f4')],
+                        names=['BDR'])
             elif 'r' in keys:
-                self._data = Table([kwargs['r']], names=['BDR'])
+                self._data = Table([np.asarray(kwargs['r'], dtype='f4')],
+                        names=['BDR'])
             elif 'iof' in keys:
-                self._data = Table([kwargs['iof']], names=['RADF'])
+                self._data = Table([np.asarray(kwargs['iof'], dtype='f4')],
+                        names=['RADF'])
             elif 'i/f' in keys:
-                self._data = Table([kwargs['i/f']], names=['RADF'])
+                self._data = Table([np.asarray(kwargs['i/f'], dtype='f4')],
+                        names=['RADF'])
             elif 'radf' in keys:
-                self._data = Table([kwargs['radf']], names=['RADF'])
+                self._data = Table([np.asarray(kwargs['radf'], dtype='f4')],
+                        names=['RADF'])
             elif 'brdf' in keys:
-                self._data = Table([kwargs['brdf']], names=['BRDF'])
+                self._data = Table([np.asarray(kwargs['brdf'], dtype='f4')],
+                        names=['BRDF'])
             elif 'reff' in keys:
-                self._data = Table([kwargs['reff']], names=['REFF'])
+                self._data = Table([np.asarray(kwargs['reff'], dtype='f4')],
+                        names=['REFF'])
             else:
                 raise ValueError('No reflectance data found.')
 
             # collect lat-lon data
             if ('geolat' in keys) & ('geolon' in keys):
-                self.geo = LatLon(kwargs['geolon'],kwargs['geolat'])
+                self.geo = LatLon(np.asarray(kwargs['geolon'], dtype='f4'),
+                                  np.asarray(kwargs['geolat'], dtype='f4'))
             else:
                 self.geo = None
 
