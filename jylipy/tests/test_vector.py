d8b6e8246a9faee204bdc69c78bb6f4815d115c6
Jian-Yang Li <jianyang2li@gmail.com>
2021-11-30 17:25:53 -0500

    update test for `EllipsoidProjection`

--- jylipy/tests/test_vector.py
+++ jylipy/tests/test_vector.py
@@ -5,6 +5,8 @@ from ..saoimage import getds9
 class TestEllipsoidProjection():
     def test_xy2lonlat_sphere(self):
         y, x = np.mgrid[0:512, 0:512]
+        y = y.astype(float)
+        x = x.astype(float)
         los_lon, los_lat, pa = np.random.rand(3)
         los_lon = los_lon * 360
         los_lat = los_lat * 180 - 90
@@ -21,6 +23,8 @@ class TestEllipsoidProjection():
 
     def test_lonlat2xy_sphere(self):
         lat, lon = np.mgrid[0:181, 0:360]
+        lat = lat.astype(float)
+        lon = lon.astype(float)
         lat = lat - 90
         los_lon, los_lat, pa = np.random.rand(3)
         los_lon = los_lon * 360
@@ -42,6 +46,8 @@ class TestEllipsoidProjection():
 
     def test_xy2lonlat_ellipsoid(self):
         y, x = np.mgrid[0:512, 0:512]
+        y = y.astype(float)
+        x = x.astype(float)
         lon, lat, pa = np.random.rand(3)
         lon = lon * 360
         lat = lat * 180 - 90
@@ -55,3 +61,34 @@ class TestEllipsoidProjection():
         ww = np.isfinite(x) & np.isfinite(x1)
         assert np.allclose(x[ww], x1[ww])
         assert np.allclose(y[ww], y1[ww])
+
+    def test_lonlat2xy_ellipsoid(self):
+        lat, lon = np.mgrid[0:181, 0:360]
+        lat = lat - 90
+        y = y.astype(float)
+        x = x.astype(float)
+        los_lon, los_lat, pa = np.random.rand(3)
+        los_lon = los_lon * 360
+        los_lat = los_lat * 180 - 90
+        pa = pa * 180
+        los = Vector(1, los_lon, los_lat, type='geo', deg=True)
+        r = [500, 450, 350]
+        pxlscl = 2
+        v = EllipsoidProjection(r, los, pxlscl, pa=pa)
+        x, y = v.lonlat2xy(lon, lat)
+        lon1, lat1 = v.xy2lonlat(x, y)
+        ww = np.isfinite(lon) & np.isfinite(lon1)
+
+        diff_lon = np.zeros_like(lon)
+        diff_lon[ww] = (lon[ww] - lon1[ww]) % 360
+        diff_lon[diff_lon > 180] -= 360
+        diff_lat = np.zeros_like(lon)
+        diff_lat[ww] = (lat[ww] - lat1[ww]) % 360
+        diff_lat[diff_lat > 180] -= 360
+
+        diff = (lon[ww] - lon1[ww]) % 360
+        diff[diff > 180] -= 360
+        assert np.allclose(diff, 0)
+        diff = (lat[ww] - lat1[ww]) % 360
+        diff[diff > 180] -= 360
+        assert np.allclose(diff, 0)
