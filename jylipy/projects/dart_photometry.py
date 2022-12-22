53f0dbab217315c772274ddcca3e981c5500aa14
Jian-Yang Li <jianyang2li@gmail.com>
2022-12-22 11:47:38 -0500

    continue update `dart_photometry`
    * package photometric calibration into an object `PhotCal`
    * allow for flexible aperture sizes defined in pixels, arcsec, and km
    * more flexible plotting options for photometric data
    * bugfixes

--- jylipy/projects/dart_photometry.py
+++ jylipy/projects/dart_photometry.py
@@ -1,12 +1,13 @@
-import numpy as np
-import os, astropy.units as u
-import matplotlib.dates as mdates
+import os, numpy as np, astropy.units as u
+import matplotlib.pyplot as plt, matplotlib.dates as mdates
+from scipy.interpolate import interp1d
 from astropy.io import ascii, fits
 from astropy.time import Time
 from astropy import table
 from photutils import CircularAperture, aperture_photometry
 from sbpy.units import VEGAmag, VEGA
 from jylipy.hst.wfc3 import UVISImage, PAM, uvis_pix
+from jylipy.plotting import pplot
 
 
 u.add_enabled_units(VEGAmag)
@@ -14,23 +15,68 @@ u.add_enabled_units(VEGA)
 
 
 visits = ['0o', '01', '02', '03', '04', '05', '06', '11', '12', '13',
-          '14', '15', '16', '17', '18', '21', '22', '23', '24']
+          '14', '15', '16', '17', '18', '21', '22', '23', '24',
+          '31', '32']
+
+throughput_data = '/Users/jyli/Work/DART/HSTGO16674/photometry/' \
+                 + 'wfc3uvis2_aper_007_syn.csv'
 
 
-# uvis2 photcal constants from
-# https://www.stsci.edu/hst/instrumentation/wfc3/data-analysis/photometric-calibration/uvis-photometric-calibration
-phocal = {'filter': 'F350LP',
-          'photplam': 5851.14829 * u.AA,
-          'photbw': 1483.01704 * u.AA,
-          'abmag': 26.936 * u.mag,
-          'vegamag': 26.78 * VEGAmag,
-          'stmag': 27.08 * u.mag,
-          'error': 0.005 * u.mag,
-          'photflam': 5.3469E-20 * u.Unit('erg cm-2 AA-1 electron-1'),
-          'err_photflam': 2.3475E-22 * u.Unit('erg cm-2 AA-1 electron-1')
-         }
-phocal['fluxzpt'] = np.round((2.5 * np.log10(phocal['photflam'].to_value(
-            'J / (m2 um electron)')) + phocal['vegamag'].value) * VEGAmag, 2)
+class PhotCal():
+    """Photometric calibration of photometric data
+
+    uvis2 photcal constants from
+    https://www.stsci.edu/hst/instrumentation/wfc3/data-analysis/photometric-calibration/uvis-photometric-calibration
+
+    """
+
+    def __init__(self):
+        # initiate calibration constants
+        setattr(self, 'throughput_data', throughput_data)
+        setattr(self, 'filter', 'F350LP')
+        setattr(self, 'photflam', 5851.14829 * u.AA)
+        setattr(self, 'photbw', 1483.01704 * u.AA)
+        setattr(self, 'abmag', 26.936 * u.mag)
+        setattr(self, 'vegamag', 26.78 * VEGAmag)
+        setattr(self, 'stmag', 27.08 * u.mag)
+        setattr(self, 'error', 0.005 * u.mag)
+        setattr(self, 'photflam', 5.3469E-20 * u.Unit(
+            'erg cm-2 AA-1 electron-1'))
+        setattr(self, 'err_photflam', 2.3475E-22 * u.Unit(
+            'erg cm-2 AA-1 electron-1'))
+        setattr(self, 'fluxzpt', np.round((2.5 * np.log10(
+            self.photflam.to_value('J / (m2 um electron)'))
+            + self.vegamag.value) * VEGAmag, 2))
+        # aperture correction
+        ee = ascii.read(self.throughput_data)
+        ee = ee[ee['FILTER'] == 'F350LP']  # only filter F350LP
+        ee.remove_columns(['FILTER', 'WAVELENGTH'])
+        col = ee.keys()
+        apt = [float(x.split('#')[1]) for x in col]
+        ee_val = np.asarray(ee).view(dtype='float')
+        setattr(self, 'eefunc',
+            interp1d(apt, ee_val, kind='cubic', fill_value='extrapolate'))
+
+    @u.quantity_input(apt=u.arcsec, equivalencies=uvis_pix())
+    def ee(self, apt):
+        """enclosed energy function"""
+        return np.clip(self.eefunc(u.Quantity(apt, u.arcsec).value), None, 1)
+
+    def plot_ee(self):
+        """plot enclosed energy"""
+        plt.figure(figsize=(5, 3.5))
+        r = np.logspace(np.log10(0.04), np.log10(6), 100) * u.arcsec
+        plt.plot(r, self.ee(r))
+        plt.legend(['F350LP'])
+        pplot(xscl='log', xlabel='Aperture Radius (")',
+            ylabel='Enclosed Energy')
+        plt.grid()
+
+    def print(self):
+        """Print all calibration constants"""
+        for k in ['filter', 'photflam', 'photbw', 'abmag', 'vegamag',
+                  'stmag', 'error', 'photflam', 'err_photflam', 'fluxzpt']:
+            print('{} = {}'.format(k, getattr(self, k)))
 
 
 class PhotometryError(Exception):
@@ -58,6 +104,7 @@ class Photometry():
     mag, mag_err : Quantity
         Integrated magnitude and error.  Added by `.photometric_cal()`.
     """
+
     def __init__(self, visits=visits,
                  datapath=os.path.join('..', 'data'),
                  ctfile='meta/centroid_{}.csv',
@@ -84,6 +131,25 @@ class Photometry():
         self.info = info
         self.fields = []
     
+    @property
+    def _aperture_in_pix(self):
+        """Convert aperture size as scalors in pixels
+
+        Returns an array of shape (M, N), where M = len(self.aperture),
+        N = len(self.info).
+        """
+        unit = getattr(self.aperture, 'unit', u.dimensionless_unscaled)
+        vec = np.full(len(self.info), 1)
+        if unit == u.dimensionless_unscaled:
+            return np.outer(getattr(self.aperture, value, self.aperture), vec)
+        if unit.is_equivalent('pix'):
+            return np.outer(self.aperture.to_value('pix'), vec)
+        if unit.is_equivalent('arcsec'):
+            return np.outer(self.aperture.to_value('pix', uvis_pix()), vec)
+        if unit.is_equivalent('km'):
+            return np.outer(self.aperture, 1 / self.info['range']).to(
+                'arcsec', u.dimensionless_angles()).to_value('pix', uvis_pix())
+
     @u.quantity_input(aperture=[u.pix, u.km])
     def measure(self, aperture=np.linspace(1, 130, 130) * u.pix):
         """Measure aperture photometry
@@ -104,7 +170,8 @@ class Photometry():
         errs = []
         # maximum possible aperture radius, determined by the position of target
         max_r = []
-        for r in self.info:
+        apts = self._aperture_in_pix
+        for i, r in enumerate(self.info):
 
             im = fits.open(os.path.join(self.datapath,
                                         r['file'].replace('_flt', '_flc')))
@@ -119,11 +186,7 @@ class Photometry():
             data = im['sci'].data * pam_arr
             err = im['err'].data * pam_arr
             # process aperture size
-            if aperture.unit.is_equivalent('km'):
-                apt = (aperture / r['range']).to_value('arcsec',
-                            u.dimensionless_angles()) / 0.04
-            else:
-                apt = aperture.value
+            apt = apts[:, i]
             # measure photometry
             pho = table.vstack([aperture_photometry(data,
                         CircularAperture([r['xc'], r['yc']], i)) for i in apt])
@@ -147,40 +210,42 @@ class Photometry():
     def photometric_cal(self):
         """Photometric calibration
         """
+        phocal = PhotCal()
+        apts = self._aperture_in_pix
         # background removal
-        self.counts -= u.Quantity(np.outer(np.pi * self.aperture.value**2,
-                                  self.info['background']), u.electron)
-        self.ct_err =  np.sqrt(self.ct_err**2 + u.Quantity(np.outer(
-            np.pi * self.aperture.value**2, self.info['background_error']),
-            u.electron)**2)
+        bg = u.Quantity(np.pi * apts**2 * self.info['background'], u.electron)
+        self.counts -= bg
+        bg_err = u.Quantity(np.pi * apts**2 * self.info['background_error'],
+            u.electron)
+        self.ct_err =  np.sqrt(self.ct_err**2 + bg_err**2)
         
         # normalize exposure time
         self.counts /= u.Quantity(self.info['exptime'], u.s)
         self.ct_err /= u.Quantity(self.info['exptime'], u.s)
                 
         # convert to flux
-        self.flux = (self.counts * phocal['photflam']).to('W / m2 um')
-        self.flux_err = (self.ct_err * phocal['photflam']).to('W / m2 um')
+        self.flux = (self.counts * phocal.photflam).to('W / m2 um')
+        self.flux_err = (self.ct_err * phocal.photflam).to('W / m2 um')
         
         # convert to vega mag
         self.mag = u.Quantity(-2.5 * np.log10(self.counts.value), u.mag) \
-                   + phocal['vegamag']
+                   + phocal.vegamag
         self.mag_err =u.Quantity( -2.5 * np.log10(self.ct_err / self.counts
                         + 1).value, u.mag)
 
         # aperture correction for pre-impact point source
         # nn = 4  # 4 pre-impact images
-        ee_corr = ee(self.aperture.to('arcsec', uvis_pix()))
-        self.counts_apc = (self.counts.T / ee_corr).T
-        self.ctapc_err = (self.ct_err.T / ee_corr).T
+        ee_corr = phocal.ee((apts * u.pix).to('arcsec', uvis_pix()))
+        self.counts_apc = self.counts / ee_corr
+        self.ctapc_err = self.ct_err / ee_corr
 
         # convert to flux
-        self.flux_apc = (self.counts_apc * phocal['photflam']).to('W / m2 um')
-        self.fluxapc_err = (self.ctapc_err * phocal['photflam']).to('W / m2 um')
+        self.flux_apc = (self.counts_apc * phocal.photflam).to('W / m2 um')
+        self.fluxapc_err = (self.ctapc_err * phocal.photflam).to('W / m2 um')
         
         # convert to vega mag
         self.mag_apc = u.Quantity(-2.5 * np.log10(self.counts_apc.value),
-                    u.mag) + phocal['vegamag']
+                    u.mag) + phocal.vegamag
         self.magapc_err =u.Quantity( -2.5 * np.log10(self.ctapc_err /
                     self.counts + 1).value, u.mag)
         
@@ -243,18 +308,30 @@ class Photometry():
                 fld = f_[n+1].header['extname'].lower()
                 setattr(obj, fld, value)
                 obj.fields.append(fld)
+            obj.info = table.QTable.read(f_['info'])
         obj.fields.remove('info')
-        obj.info = table.Table(obj.info)
         return obj
-            
-    def plot(self, aperture=None, apc=True, figsize=(6, 4)):
-        # plot aperture photometry
-        if aperture is None:
-            plot_apt = self.aperture
-        else:
-            plot_apt = aperture
+
+    def plot(self, aperture_index=None, apc=True, figsize=(6, 4),
+            legend_unit=None, equivalencies=uvis_pix()):
+        """plot aperture photometry
+
+        aperture_index : int or array of int
+            Index of apertures to be plotted
+        apc : bool
+            Use aperture corrected data
+        figsize : tuple
+            Figure size
+        legend_unit : str or u.Unit
+            Aperture size unit in the plot legend.  Must be equivalent
+            to `self.aperture.unit`.
+        equivalencies : list
+            Unit equivalencies to convert aperture size unit
+        """
+        if aperture_index is None:
+            aperture_index = range(len(self.aperture))
         
-        short = self.info['exptime'] < 20
+        short = self.info['exptime'] < 20 * u.s
         tt = Time(self.info['utc-mid'][short])
         if apc:
             mag_plot = self.mag_apc[:, short]
@@ -264,13 +341,16 @@ class Photometry():
             mag_err = self.mag_err[:, short]
         
         f, ax = plt.subplots(figsize=figsize)
-        for i in np.asarray(plot_apt, dtype=int) - 1:
+        for i in aperture_index:
             #plt.plot_date(tt.plot_date, mag_plot[i], 'o', mfc='none')
             ax.errorbar(tt.plot_date, mag_plot[i].value, mag_err[i].value,
                 fmt='o', mfc='none')
-        ax.legend(['{}'.format(x) for x in (plot_apt * u.pix).to('arcsec',
-                uvis_pix())], ncol=2, loc='upper right')
-        jp.pplot(ax, ylabel=self.mag.unit, skipline=True, ylim=[15, 13.2])
+        if legend_unit is None:
+            legend_unit = getattr(self.aperture, 'unit', '')
+        ax.legend(['{:.2f}'.format(x) for x in
+            self.aperture[aperture_index].to(legend_unit,
+                equivalencies)], ncol=2, loc='upper right')
+        pplot(ax, ylabel=self.mag.unit, skipline=True, ylim=[15, 13.2])
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%dT%H:%M'))
         _ = plt.xticks(rotation=45)
         ax.grid()
