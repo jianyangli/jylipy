a8ecf3adbf7881dd9df6d4aac2052e308d9ec15a
Jian-Yang Li <jianyang2li@gmail.com>
2023-04-20 13:16:05 -0400

    add untouched region to `CRRej`
    The region in the center is protected from any cosmic ray rejection.

--- jylipy/projects/dart_data.py
+++ jylipy/projects/dart_data.py
@@ -9,25 +9,48 @@ class CRRej():
     """
     
     def __init__(self, sigclip=4, cleantype='medmask', niter=10,
-                 masks=[4, 256, 512]):
+                 masks=[4, 256, 512], untouch=0):
         """
         sigclip, cleantype, niter
             Parameters for `ccdproc.cosmicray_lacosmic`
         mask : list of int, optional
             The UVIS DQ mask values to be used for the cosmic ray
             rejection
+        untouch : int, optional
+            The side of the center box that is untouched by cosmic ray
+            rejection process.
         """
         self.sigclip = sigclip
         self.cleantype = cleantype
         self.niter = niter
         self.masks = masks
+        self.untouch = untouch
 
-    def clean(self, infile, verbose=True):
+    def clean(self, infile, centroid=None, verbose=True):
+        """
+        infile : str
+            Name of input fits file
+        centroid : np.array of shape (2,), optional
+            Specify the centroid (y, x).  Used when `.untouch > 0`.
+            If `.untouch > 0` but `centroid == None`, then a
+            ValueError will be raised.
+        verbose : bool, optional
+            Verbose mode
+        """
         # load image
         im = CCDData.read(infile)
         with fits.open(infile) as f_:
             dq = f_['dq'].data
             rn = f_[0].header['PCTERNOI']
+        if self.untouch > 0:
+            if centroid is None:
+                raise ValueError('Centroid is not specified')
+            halfside = self.untouch // 2
+            y1 = int(centroid[0]) - halfside
+            y2 = int(centroid[0]) + halfside + 1
+            x1 = int(centroid[1]) - halfside
+            x2 = int(centroid[1]) + halfside + 1
+            box = im.data[y1:y2, x1:x2]
         
         # generate mask from dq array
         mask = np.zeros_like(dq)
@@ -43,6 +66,12 @@ class CRRej():
         crmask = self.cleaned.mask
         crmask[mask > 0] = False
         self.cleaned.crmask = crmask
+
+        # recover the center region
+        if self.untouch > 0:
+            #return box, y1, y2, x1, x2
+            self.cleaned.data[y1:y2, x1:x2] = box
+            self.cleaned.crmask[y1:y2, x1:x2] = False
 
     def write(self, outfile, infile=None, overwrite=False):
         """
