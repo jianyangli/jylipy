a10e15afc0e7c836eb11b4b249fe78fbe8eced70
Jian-Yang Li <jianyang2li@gmail.com>
2023-07-07 22:20:58 -0400

    use `NEATM` as the base class of `STM`

--- jylipy/thermal/stm.py
+++ jylipy/thermal/stm.py
@@ -9,10 +9,11 @@ import astropy.units as u
 import astropy.constants as const
 from sbpy.bib import cite
 from .core import ThermalModelABC
+from .neatm import NEATM
 
 __all__ = ['STM']
 
-class STM(ThermalModelABC):
+class STM(ThermalModelABC, NEATM):
     """Standard thermal model"""
 
     @cite({'method': '1986Icar...68..239L'})
@@ -33,38 +34,12 @@ class STM(ThermalModelABC):
         beaming = 0.756
         super().__init__(rh, R, albedo, emissivity, beaming)
 
-    @property
-    def Tss(self):
-        """Subsolar temperature"""
-        f_sun = const.L_sun / (4 * np.pi * self.rh**2)
-        return (((1 - self.albedo) * f_sun / (self.beaming * self.emissivity
-            * const.sigma_sb)) ** 0.25).decompose()
-
-    @u.quantity_input(lon=u.deg, lat=u.deg)
-    def T(self, lon, lat):
-        """Surface temperature at specific (lat, lon)
-
-        lon : u.Quantity in units equivalent to deg
-            Longitude
-        lat : u.Quantity in units equivalent to deg
-            Latitude
-
-        Returns
-        -------
-        u.Quantity : Surface temperature.
-        """
-        coslon = np.cos(lon)
-        coslat = np.cos(lat)
-        prec = np.finfo(coslat.value).resolution
-        if (abs(coslon) < prec) or (abs(coslat) < prec) or (coslon < 0):
-            return 0 * u.K
-        else:
-            return self.Tss * (coslon * coslat)**0.25
-
-    @u.quantity_input(phase=u.deg)
-    def fluxd(self, wave_freq, delta, phase=0*u.deg, **kwargs):
+    @u.quantity_input(phase=u.deg, phase_slope=u.mag/u.deg)
+    def fluxd(self, wave_freq, delta, phase=0*u.deg,
+            phase_slope=0.01*u.mag/u.deg, **kwargs):
         """Calculate total flux density.
         """
+        scl = u.Magnitude((phase * phase_slope).to_value('mag')).physical
         sublon = 0. * u.deg
         sublat = 0. * u.deg
-        return super().fluxd(wave_freq, delta, sublon, sublat, **kwargs)
+        return super().fluxd(wave_freq, delta, sublon, sublat, **kwargs) * scl
